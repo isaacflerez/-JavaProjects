b060b90d7ea32efbee694ada29e00a128e713cf1
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-23 09:31:30 -0500

    Add files via upload

---
+++ primeros codigos/Tree.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tree;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Tree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey there, I’m Henry and I’m learning to code in Java! secso");
+        System.out.println("    x   ");
+        System.out.println("   xxx  ");
+        System.out.println("xxxxxxxxx");
+        System.out.println("    x   ");
+        System.out.println("    x   ");
+        System.out.println("    x   ");
+        
+       
+    }
+    
+}
