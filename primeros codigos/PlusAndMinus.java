9e378bfa337cfd5350e10da53499c40d273a7ca7
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-23 09:47:12 -0500

    Add files via upload

---
+++ primeros codigos/PlusAndMinus.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package plusandminus;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class PlusAndMinus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int giraffesInZoo= 4;
+        int ZebrasInZoo=8;
+        int animalsInZoo = giraffesInZoo + ZebrasInZoo;
+        
+        System.out.println(animalsInZoo);
+        
+        
+        int numberZebrasAfterTrade = ZebrasInZoo - 2;
+        
+        System.out.println(numberZebrasAfterTrade);
+        // TODO code application logic here
+    }
+    
+}
