16dbd94bacb2333303659aea3ff0374d0dad5103
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-23 08:58:02 -0500

    Add files via upload

--- primeros codigos/Booleans.java
+++ primeros codigos/Booleans.java
@@ -15,6 +15,9 @@ public class Booleans {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean intsCanHoldDecimals = false;
+        System.out.println(intsCanHoldDecimals);
+        
     }
     
 }
