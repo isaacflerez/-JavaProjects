161c1f779fff2dbdc222e27eb8bb168df401adf4
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-23 09:32:09 -0500

    Add files via upload

--- primeros codigos/Tree.java
+++ primeros codigos/Tree.java
@@ -14,7 +14,7 @@ public class Tree {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hey there, I’m Henry and I’m learning to code in Java! secso");
+        System.out.println("Hey there, I’m Henry and I’m learning to code in Java!");
         System.out.println("    x   ");
         System.out.println("   xxx  ");
         System.out.println("xxxxxxxxx");
