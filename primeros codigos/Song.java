7d37605ca0af347b32388cac69a2404db7d9c196
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:28:19 -0500

    Add files via upload

---
+++ primeros codigos/Song.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package song;
+
+/**
+ *
+ * @author user
+ */
+public class Song {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+        String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+        String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+
+        System.out.println(line1.equals(line2));
+        System.out.println(line2.equals(line3));
+    }
+    
+}
