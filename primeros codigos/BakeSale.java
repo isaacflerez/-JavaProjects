4cbb97412cf8a9856bc12b73de0a235e8baef8f3
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:23:34 -0500

    Add files via upload

---
+++ primeros codigos/BakeSale.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bakesale;
+
+
+public class BakeSale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numCookies = 17;
+        numCookies -= 3;
+        System.out.println(numCookies);
+        numCookies /= 2;
+    
+        System.out.println(numCookies);
+    }
+    
+}
