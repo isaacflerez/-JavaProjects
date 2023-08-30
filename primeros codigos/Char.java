2c9ef93373bdd09f3417a7d8908eabeaaad02ba4
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:20:39 -0500

    Add files via upload

---
+++ primeros codigos/Char.java
@@ -0,0 +1,12 @@
+
+package pkgchar;
+
+public class Char {
+
+ 
+    public static void main(String[] args) {
+        char expectedGrade = 'C'; 
+        System.out.println(expectedGrade);
+    }
+    
+}
