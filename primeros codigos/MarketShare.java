d0f765a3796b7a50aee0aeeb5e0758a7953276a1
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:26:53 -0500

    Add files via upload

---
+++ primeros codigos/MarketShare.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package marketshare;
+
+/**
+ *
+ * @author user
+ */
+public class MarketShare {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double androidShare = -81.7;
+     
+   
+    System.out.println(androidShare);
+    }
+    
+}
