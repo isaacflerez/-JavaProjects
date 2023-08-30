91ae81710e6e7b632e94bd63b7b2c453e9b0b7dc
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:49:14 -0500

    Add files via upload

---
+++ primeros codigos/Creator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.creator;
+
+/**
+ *
+ * @author user
+ */
+public class Creator {
+
+    public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;      //The int data type allows values between -2,147,483,648 and 2,147,483,647, inclusive.
+        System.out.println(name);
+        System.out.println(yearCreated);
+    }
+}
