9bfce7055c311c5635e9144d202a2ffdc83e1080
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:25:35 -0500

    Add files via upload

---
+++ primeros codigos/Zoo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package zoo;
+
+/**
+ *
+ * @author user
+ */
+public class Zoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int animals = 12;
+        String species = "zebra";
+        String zooDescription = "Our zoo has " + animals + " " + species + "s!"; 
+        System.out.println(zooDescription);
+    }
+    
+}
