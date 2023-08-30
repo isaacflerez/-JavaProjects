001b3b46125d0402cf8723ad41452361833059fb
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:35:02 -0500

    Add files via upload

---
+++ primeros codigos/Modulo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modulo;
+
+/**
+ *
+ * @author user
+ */
+public class Modulo {
+
+    public static void main(String[] args) {
+        int students = 26;
+    int leftOut = students%3; 
+
+    System.out.println(leftOut);
+    }
+}
