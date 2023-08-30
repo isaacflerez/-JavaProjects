077be185ec39cca17b736a423f797cf216416b25
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:41:39 -0500

    Add files via upload

---
+++ primeros codigos/Operations.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operations;
+
+/**
+ *
+ * @author user
+ */
+public class Operations {
+
+    public static void main(String[] args) {
+        int expression1 = 5 % 2 - (4 * 2 - 1);
+    
+
+        int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+    
+
+        int expression3 = 5 * 4 % 3 - 2 + 1;
+        
+        System.out.println(expression1);
+        System.out.println(expression2);
+        System.out.println(expression3);
+        System.out.println(expression1 + expression2 + expression3);
+    
+    }
+}
