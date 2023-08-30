ee49d83bdc5063f4dd8038100ec43179436896a8
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:48:13 -0500

    Add files via upload

---
+++ primeros codigos/Song2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package song2;
+
+/**
+ *
+ * @author user
+ */
+public class Song2 {
+
+    public static void main(String[] args) {
+        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+        System.out.println(openingLyrics);
+    }
+}
