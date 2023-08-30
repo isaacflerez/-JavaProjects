5821be4d3f38de47fcaca7ca044cc8d2bfcf3f2f
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-08-29 19:31:11 -0500

    Add files via upload

---
+++ primeros codigos/Final.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author user
+ */
+public class Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        System.out.println(pi);  
+        // Final para no volver a asignarle otro valor a pi 
+    }
+    
+}
