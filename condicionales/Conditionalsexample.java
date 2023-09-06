2999e7da523d72226904613d7f3f02abc0110529
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-09-06 09:40:29 -0500

    Add files via upload

---
+++ condicionales/Conditionalsexample.java
@@ -0,0 +1,18 @@
+
+package conditionalsexample;
+
+
+public class Conditionalsexample {
+
+    
+    public static void main(String[] args) {
+        int x = 10; 
+        int y  = 12;
+        
+        if(x+y>20){
+            System.out.println("x + y es mas grande que 20");
+        }
+
+    }
+    
+}
