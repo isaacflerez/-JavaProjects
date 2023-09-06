e8c8d04862cdd11fe3a22ae651e92e699a147e09
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-09-06 09:45:24 -0500

    Add files via upload

---
+++ condicionales/Conditionalexample2.java
@@ -0,0 +1,20 @@
+
+package conditionalexample2;
+
+
+public class Conditionalexample2 {
+
+    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 12;
+        
+        if(x+y<10){
+            System.out.println("x + y es menor que 10");
+        }
+        else{
+            System.out.println("x + y es mayor que 10");
+        }
+    }
+    
+}
