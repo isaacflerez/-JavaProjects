0118de6759a0799c1c9b3c66362b46803c6e594b
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-10-05 09:59:20 -0500

    Add files via upload

---
+++ SegundoCorte/Herencia/TestHerencia.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import herencia.*;
+
+public class TestHerencia {
+
+    
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Axel Roel", 'm', 25, "Santo tomas");
+        
+        System.out.println(persona1);
+    }
+    
+}
