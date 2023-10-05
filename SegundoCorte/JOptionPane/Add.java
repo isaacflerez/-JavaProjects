773d39248d27fdc599c8ead43b29df1142e1cc85
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-10-05 08:53:27 -0500

    Add files via upload

---
+++ SegundoCorte/JOptionPane/Add.java
@@ -0,0 +1,26 @@
+package add;
+
+import javax.swing.JOptionPane;
+
+public class Add {
+
+    
+    public static void main(String[] args) {
+        String value1;
+        String value2;
+        int result, number1, number2;
+        
+        value1=JOptionPane.showInputDialog("Enter first number");
+        value2=JOptionPane.showInputDialog("Enter second number");
+        
+        number1=Integer.parseInt(value1);
+        number2=Integer.parseInt(value2);
+        
+        result = number1 + number2;
+        
+        JOptionPane.showInputDialog(null, "Result is: "+ result, "Result", JOptionPane.PLAIN_MESSAGE);
+        
+        System.exit(0);
+    }
+    
+}
