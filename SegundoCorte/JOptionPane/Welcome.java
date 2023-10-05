c1c75bfe9385dff245971ed562274d93d5bbb397
isaacflerez <142518069+isaacflerez@users.noreply.github.com>
2023-10-05 08:53:58 -0500

    Add files via upload

---
+++ SegundoCorte/JOptionPane/Welcome.java
@@ -0,0 +1,11 @@
+package welcome;
+import javax.swing.JOptionPane;
+public class Welcome {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "¡Welcome\nto\nthe java world!");
+        System.exit(0);
+    }
+    
+}
